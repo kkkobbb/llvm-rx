c15016b83d8229e50e167dbdf52deb1310c518a6
koba <38810208+kkkobbb@users.noreply.github.com>
2020-08-08 22:50:51 +0900

    テスト用コード修正

--- tests/asm/start.s
+++ tests/asm/start.s
@@ -7,21 +7,21 @@ _start:  ; 0x10000000 に割り当てられるはず
 	mvtc #0xffffff00, isp
 	; ユーザースタックポインタの初期化
 	mvtc #0xf0000000, usp
-	; INT命令用ベクタテーブルの設定
+	; INT命令用ベクタテーブルの初期化
 	mvtc #inttable, intb
 
-	; スタックにユーザモード用のpsw,pcをセットする
+	; スタックにユーザモード用のpsw,pcを積む
 	mvfc psw, r1
 	or #0x00100000, r1  ; ユーザーモードにする (PMビットを1にする)
 	push.l r1
-	mov.l #usermode_code, r1  ; ユーザーモードで実行する場所
+	mov.l #start_usermode, r1  ; ユーザーモードで実行する場所
 	push.l r1
 
 	; ユーザーモードに移行
 	rte
 
-usermode_code:
-	; 各レジスタの初期化
+start_usermode:
+	; 外部関数実行前の初期化
 	xor r1, r1
 	xor r2, r2
 	xor r3, r3
@@ -38,11 +38,15 @@ usermode_code:
 	xor r14, r14
 	xor r15, r15
 
+before_run:  ; 外部関数実行前にレジスタ等を変更するためのブレークポイント用
 	; 外部関数実行
 	bsr run
 
 	; ユーザーモードなのでINT命令で特権モードにして、wait実行して終了
 	int #0
+
+
+; 以降、ベクタテーブルの設定
 
 int0:
 	wait  ; 停止 (特権モードが必要)
