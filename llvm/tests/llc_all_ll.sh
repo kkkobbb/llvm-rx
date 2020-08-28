1a0a06564d986a180d5489bf6218e1ee41144213
koba <38810208+kkkobbb@users.noreply.github.com>
2020-08-28 20:24:06 +0900

    テスト用ツール修正

--- llvm/tests/llc_all_ll.sh
+++ llvm/tests/llc_all_ll.sh
@@ -2,7 +2,20 @@
 
 RXLLC="${HOME}/llvm-project/_build_rx/bin/llc"
 
-for llfile in ./*.ll; do
-	echo "llc $llfile"
-	$RXLLC $llfile
+echo "$RXLLC\n"
+
+TEST_NUM=0
+ERROR_NUM=0
+for llfile in *.ll; do
+	TEST_NUM=$((TEST_NUM+1))
+	ERR_MSG=$($RXLLC --fatal-warnings $llfile 2>&1)
+	if [ $? -ne 0 ]; then
+		ERROR_NUM=$((ERROR_NUM+1))
+		echo "Error: $llfile"
+		#echo "$ERR_MSG"
+	else
+		echo "Ok:    $llfile"
+	fi
 done
+
+echo "\nError: $ERROR_NUM/$TEST_NUM"
