72556a07d77022c633b38df2ddde60d791304928
koba <38810208+kkkobbb@users.noreply.github.com>
2020-10-20 13:43:09 +0900

    setccに対応

--- llvm/RX/RXExpandPseudoInsts.cpp
+++ llvm/RX/RXExpandPseudoInsts.cpp
@@ -47,6 +47,8 @@ private:
                 MachineBasicBlock::iterator &NextMBBI);
   bool expandBrcond(MachineBasicBlock &MBB, MachineBasicBlock::iterator MBBI,
                     int branchOp, MachineBasicBlock::iterator &NextMBBI);
+  bool expandStoreCond(MachineBasicBlock &MBB, MachineBasicBlock::iterator MBBI,
+                       int scOp, MachineBasicBlock::iterator &NextMBBI);
 };
 
 char RXExpandPseudo::ID = 0;
@@ -64,6 +66,7 @@ bool RXExpandPseudo::expandMBB(MachineBasicBlock &MBB) {
 
   MachineBasicBlock::iterator MBBI = MBB.begin(), E = MBB.end();
   while (MBBI != E) {
+    LLVM_DEBUG(dbgs() << "### expandMBB " << *MBBI << "\n");
     MachineBasicBlock::iterator NMBBI = std::next(MBBI);
     Modified |= expandMI(MBB, MBBI, NMBBI);
     MBBI = NMBBI;
@@ -96,40 +99,88 @@ bool RXExpandPseudo::expandMI(MachineBasicBlock &MBB,
     return expandBrcond(MBB, MBBI, RX::BGE, NextMBBI);
   case RX::pBRCOND_UGE:
     return expandBrcond(MBB, MBBI, RX::BGEU, NextMBBI);
+
+  case RX::pSETCC_EQ:
+    return expandStoreCond(MBB, MBBI, RX::SCEQ, NextMBBI);
+  case RX::pSETCC_NE:
+    return expandStoreCond(MBB, MBBI, RX::SCNE, NextMBBI);
+  case RX::pSETCC_LT:
+    return expandStoreCond(MBB, MBBI, RX::SCLT, NextMBBI);
+  case RX::pSETCC_ULT:
+    return expandStoreCond(MBB, MBBI, RX::SCLTU, NextMBBI);
+  case RX::pSETCC_GT:
+    return expandStoreCond(MBB, MBBI, RX::SCGT, NextMBBI);
+  case RX::pSETCC_UGT:
+    return expandStoreCond(MBB, MBBI, RX::SCGTU, NextMBBI);
+  case RX::pSETCC_LE:
+    return expandStoreCond(MBB, MBBI, RX::SCLE, NextMBBI);
+  case RX::pSETCC_ULE:
+    return expandStoreCond(MBB, MBBI, RX::SCLEU, NextMBBI);
+  case RX::pSETCC_GE:
+    return expandStoreCond(MBB, MBBI, RX::SCGE, NextMBBI);
+  case RX::pSETCC_UGE:
+    return expandStoreCond(MBB, MBBI, RX::SCGEU, NextMBBI);
   }
 
   return false;
 }
 
+static void buildCmp(const RXInstrInfo *TII, MachineBasicBlock &MBB,
+                     MachineBasicBlock::iterator MBBI, DebugLoc DL,
+                     MachineOperand operand1, MachineOperand operand2) {
+  if (operand1.isReg()) {
+    // cmp reg reg
+    BuildMI(MBB, MBBI, DL, TII->get(RX::CMP_RR))
+      .addReg(operand1.getReg())
+      .addReg(operand2.getReg());
+  } else {
+    // cmp imm reg
+    auto operandVal = operand1.getImm();
+    auto opcode = RX::CMP_I32R;
+    // 4bit即値の比較が使える場合は使う
+    if (operandVal >= 0 && operandVal <=15)
+      opcode = RX::CMP_UI4R;
+
+    BuildMI(MBB, MBBI, DL, TII->get(opcode))
+      .addImm(operandVal)
+      .addReg(operand2.getReg());
+  }
+}
+
 bool RXExpandPseudo::expandBrcond( MachineBasicBlock &MBB,
     MachineBasicBlock::iterator MBBI, int branchOp,
     MachineBasicBlock::iterator &NextMBBI) {
-  LLVM_DEBUG(dbgs() << "### expandBrcond " << MBB << "\n");
+  LLVM_DEBUG(dbgs() << "### expandBrcond " << *MBBI << "\n");
 
   MachineInstr &MI = *MBBI;
   DebugLoc DL = MI.getDebugLoc();
 
   // pBRCOND_*のオペランドは(reg reg)か(imm reg)の2種類
-  if (MI.getOperand(0).isReg()) {
-    BuildMI(MBB, MI, DL, TII->get(RX::CMP_RR))
-      .addReg(MI.getOperand(0).getReg())
-      .addReg(MI.getOperand(1).getReg());
-  } else {
-    auto oprand0 = MI.getOperand(0).getImm();
-    auto opcode = RX::CMP_I32R;
-    // 4bit即値の比較が使える場合は使う
-    if (oprand0 >= 0 && oprand0 <=15)
-      opcode = RX::CMP_UI4R;
+  buildCmp(TII, MBB, MBBI, DL, MI.getOperand(0), MI.getOperand(1));
 
-    BuildMI(MBB, MI, DL, TII->get(opcode))
-      .addImm(oprand0)
-      .addReg(MI.getOperand(1).getReg());
-  }
-
-  BuildMI(MBB, MI, DL, TII->get(branchOp))
+  BuildMI(MBB, MBBI, DL, TII->get(branchOp))
       .addMBB(MI.getOperand(2).getMBB());
 
   NextMBBI = MBB.end();
+  MI.eraseFromParent();
+  return true;
+}
+
+bool RXExpandPseudo::expandStoreCond(MachineBasicBlock &MBB,
+    MachineBasicBlock::iterator MBBI, int scOp,
+    MachineBasicBlock::iterator &NextMBBI) {
+  LLVM_DEBUG(dbgs() << "### expandStoreCond " << *MBBI << "\n");
+
+  MachineInstr &MI = *MBBI;
+  DebugLoc DL = MI.getDebugLoc();
+
+  // pSETCC_*のオペランドは(reg reg reg)か(reg imm reg)の2種類
+  buildCmp(TII, MBB, MBBI, DL, MI.getOperand(1), MI.getOperand(2));
+
+  BuildMI(MBB, MBBI, DL, TII->get(scOp))
+      .addReg(MI.getOperand(0).getReg(), RegState::Define);
+
+  //NextMBBI = MBB.end();
   MI.eraseFromParent();
   return true;
 }
---
+++ llvm/test/CodeGen/RX/setcc.ll
@@ -0,0 +1,12 @@
+; RUN: llc -mtriple=rxv1 -verify-machineinstrs < %s | FileCheck %s
+
+define i32 @setcc(i32 %a, i32 %b) noinline optnone {
+; CHECK-LABEL: setcc:
+; CHECK:      ; %bb.0:
+; CHECK-NEXT:   cmp r1, r2
+; CHECK-NEXT:   sceq r1
+; CHECK-NEXT:   rts
+  %cmp = icmp eq i32 %a, %b
+  %ret = zext i1 %cmp to i32
+  ret i32 %ret
+}
